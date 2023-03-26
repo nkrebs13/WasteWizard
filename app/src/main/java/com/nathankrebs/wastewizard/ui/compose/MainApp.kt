e093365e6688bbb57064c2d045c1aedaf3cc7fcc
Nathan Krebs <nathan.krebs@deliverhealth.com>
2023-03-26 15:55:17 -0700

    Reorganize, refactor, and clean-up for navigation

--- app/src/main/java/com/nathankrebs/wastewizard/ui/compose/MainApp.kt
+++ app/src/main/java/com/nathankrebs/wastewizard/ui/compose/MainApp.kt
@@ -2,6 +2,8 @@ package com.nathankrebs.wastewizard.ui.compose
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -9,31 +11,70 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.nathankrebs.wastewizard.model.DriverItem
 import com.nathankrebs.wastewizard.network.NetworkingSingleton
+import com.nathankrebs.wastewizard.ui.compose.Navigation.navToDriverRoute
 
+/**
+ * The "main" screen for the application
+ */
 @Composable
 fun MainApp(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "drivers") {
-        composable("drivers") {
-            DriverListScreen(
-                modifier = modifier,
-                onDriverClick = { driverItem ->
-                    navController.navigate("routes/${driverItem.toJsonString()}")
-                }
-            )
-        }
+    NavHost(navController = navController, startDestination = Navigation.NAVROUTE_DRIVER_LIST) {
+        driverListGraph(
+            modifier = modifier,
+            onDriverClick = { driverItem -> navController.navToDriverRoute(driverItem) }
+        )
+        driverRouteGraph(modifier)
+    }
+}
 
-        composable(
-            route = "routes/{driverItem}",
-            arguments = listOf(navArgument("driverItem") { type = NavType.StringType })
-        ) { backStackEntry ->
-            val driverItem = backStackEntry.arguments?.getString("driverItem")?.toDriverItem()
-                ?: throw IllegalArgumentException("You need to pass in a 'driverItem' argument!")
-            DriverRouteScreen(
-                modifier = modifier,
-                driverItem = driverItem,
-            )
-        }
+/**
+ * Defines some navigation-related constants and helper functions
+ */
+private object Navigation {
+    /**
+     * Route for [driverListGraph]
+     */
+    const val NAVROUTE_DRIVER_LIST = "drivers"
+
+    /**
+     * Route for [driverRouteGraph].
+     *
+     * Navigate to this with [navToDriverRoute]
+     */
+    const val NAVROUTE_DRIVER_ROUTE_LIST = "routes/{driverItem}"
+
+    /**
+     * Used to navigate to [driverRouteGraph]
+     */
+    fun NavController.navToDriverRoute(driverItem: DriverItem) {
+        this.navigate("routes/${driverItem.toJsonString()}")
+    }
+}
+
+private fun NavGraphBuilder.driverListGraph(
+    modifier: Modifier,
+    onDriverClick: (DriverItem) -> Unit,
+) {
+    composable(Navigation.NAVROUTE_DRIVER_LIST) {
+        DriverListScreen(
+            modifier = modifier,
+            onDriverClick = onDriverClick,
+        )
+    }
+}
+
+private fun NavGraphBuilder.driverRouteGraph(modifier: Modifier) {
+    composable(
+        route = Navigation.NAVROUTE_DRIVER_ROUTE_LIST,
+        arguments = listOf(navArgument("driverItem") { type = NavType.StringType })
+    ) { backStackEntry ->
+        val driverItem = backStackEntry.arguments?.getString("driverItem")?.toDriverItem()
+            ?: throw IllegalArgumentException("You need to pass in a 'driverItem' argument!")
+        DriverRouteScreen(
+            modifier = modifier,
+            driverItem = driverItem,
+        )
     }
 }
 
