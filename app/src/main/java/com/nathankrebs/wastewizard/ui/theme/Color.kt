5d37675aae8b1d6a20d907fcf0ec0ac2266da48f
Nathan Krebs <nathan.krebs@deliverhealth.com>
2023-03-26 17:13:06 -0700

    Theme and colors update

--- app/src/main/java/com/nathankrebs/wastewizard/ui/theme/Color.kt
+++ app/src/main/java/com/nathankrebs/wastewizard/ui/theme/Color.kt
@@ -1,8 +1,23 @@
 package com.nathankrebs.wastewizard.ui.theme
 
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val DarkColorPalette = darkColors(
+    primary = Color(0xFF4CAF50),
+    primaryVariant = Color(0xFF388E3C),
+    secondary = Color(0xFF448AFF),
+    secondaryVariant = Color(0xFF018786),
+    background = Color(0xFF212121),
+    surface = Color(0xFF313131),
+)
+
+val LightColorPalette = lightColors(
+    primary = Color(0xFF66BB6A),
+    primaryVariant = Color(0xFF388E3C),
+    secondary = Color(0xFF448AFF),
+    secondaryVariant = Color(0xFF018786),
+    background = Color.White,
+    surface = Color.White,
+)
--- app/src/main/java/com/nathankrebs/wastewizard/ui/theme/Theme.kt
+++ app/src/main/java/com/nathankrebs/wastewizard/ui/theme/Theme.kt
@@ -1,42 +1,29 @@
 package com.nathankrebs.wastewizard.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
 
 @Composable
-fun WasteWizardTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+fun WasteWizardTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColorPalette else LightColorPalette
     val typography = if(darkTheme) DarkTypography else LightTypography
 
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colors.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = !darkTheme
+        }
+    }
     MaterialTheme(
         colors = colors,
         typography = typography,
